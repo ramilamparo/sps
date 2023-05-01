002d9ace960d0133f06133611a2b2b86ec8cb95a
Ramil Amparo <ramilamparo@gmail.com>
2023-05-01 13:35:15 +0400

    Add font.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.scss";
+import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
